d362194161446c880f0aa42767835dba4cff190e
Bernardo Grabowski Costa <bernardo.g.costa@hotmail.com>
2022-10-21 19:12:22 -0300

    install imask

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 import "./css/index.css"
+import IMask from "imask"
+
 
 const ccBgColor01 = document.querySelector(".cc-bg svg >g g:nth-child(1) path")
 const ccBgColor02 = document.querySelector(".cc-bg svg >g g:nth-child(2) path")
@@ -18,3 +20,9 @@ function setCardType(type) {
 }
 
 globalThis.setCardType = setCardType
+
+const securityCode = document.querySelector('#security-code')
+const securityCodePattern = {
+    mask: "0000"
+}
+const securityCodeMasked = IMask(securityCode, securityCodePattern)
